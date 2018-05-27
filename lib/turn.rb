1b152f9cac30e85a2777992af236b17c2bdee97d
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-27 18:45:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,11 +17,9 @@ def position_taken?(board, index)
 end
 def valid_move?(board, index)
   
-  if position_taken?(board, index) || index < 0 || index > 8
-    return false
+  if !position_taken?(board, index) && index >= 0 && index <= 8
+    return true
   end
   
-  return true
-  
-  
+  return false
 end
